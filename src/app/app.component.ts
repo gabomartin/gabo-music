ffc62e0878517d8edff315719ab0c328c8c7581b
gabomartin <75818899+gabomartin@users.noreply.github.com>
2021-07-09 17:54:28 -0300

    Implementaciones varias

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,39 @@
 import { Component } from '@angular/core';
 
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  
+
   title = 'gabo-music';
   opened:boolean = false;
-
-
+  dj:boolean =false;
+  music:boolean =false;
+  currDiv: string = "";
   sideOpen(){
     this.opened=this.opened==false?true:false;
   }
+  
+
+  showDiv(divVal: string) {
+    this.currDiv = divVal;
+  }
+
+  linkExt(site:string){
+
+    if(site=='ig'){
+    window.open("https://www.instagram.com/gabo.martin/", "_blank");
+    }
+    if(site=='fb'){
+      window.open("https://facebook.com/GaboMartinMusic", "_blank");
+      }
+}
+
+
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -13,11 +14,13 @@ import { FormsModule } from '@angular/forms';
 
 
 
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
@@ -26,6 +29,7 @@ import { FormsModule } from '@angular/forms';
     MatIconModule,
     MatSidenavModule,
     FormsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
