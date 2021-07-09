e12bc0e572cb9c38855d5b223a72e19206920b28
gabomartin <75818899+gabomartin@users.noreply.github.com>
2021-07-09 14:51:23 -0300

    Side nav bar agregada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'gabo-music';
+  opened:boolean = false;
+
+
+  sideOpen(){
+    this.opened=this.opened==false?true:false;
+  }
 }
+
